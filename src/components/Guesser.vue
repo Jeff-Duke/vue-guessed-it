45d918639ee8f2eed86981114bd2d84178441030
Jeff Duke <Jeff.Duke@Gmail.com>
2017-12-30 10:21:10 -0700

    Final (#7)
    * update readme
    
    * remove console statement
    
    * add a couple basic tests
    
    * rename test spec file and get a couple basic tests working
    
    * add more tests and test rando number is between range
    
    * refactor methods
    
    * add styling
    
    * final touchups

--- src/components/Guesser.vue
+++ src/components/Guesser.vue
@@ -1,111 +1,218 @@
 <template>
   <section class="guesser-container">
-    <h1 class="message">{{ msg }}</h1>
-    <h3>Enter a guess between {{min}} and {{max}}</h3>
-    <article class="inputs-buttons">
-      <input :min="min" :max="max" type="number" v-on:keyup.enter="submitGuess" class="guess-input" placeholder="enter your guess" v-model="userGuess" />
-      <button id="submitButton" v-on:click="submitGuess" :disabled="!userGuess">Submit</button>
-      <button id="clearButton" v-on:click="clearInput" :disabled="!userGuess">clear</button>
-      <button id="resetButton" v-on:click="resetGame" :disabled="level <= 1">reset</button>
+    <article class="message">
+      <h1 class="title">
+        Number <span>Guesser</span>
+      </h1>
+
+      <p class="range">
+        Enter a guess between {{min}} and {{max}}
+      </p>
     </article>
-    <label for="minInput">Min</label>
-    <input id="minInput" type="number" v-model="min" v-on:blur="getNewRandom" />
-    <label for="maxInput">Max</label>
-    <input id="maxInput" type="number" v-model="max" v-on:blur="getNewRandom" />
-    <h3 v-if="guessResult">{{ guessResult }}</h3>
-    <h3 v-if="lastGuess">your last guess was: {{lastGuess}} </h3>
-    <h4>{{userGuess}}</h4>
+    <article class="game-body container">
+
+      <input
+        v-model="userGuess"
+        type="number"
+        @keyup.enter="submitGuess"
+        class="input input--guess"
+        placeholder="enter your guess"
+        ref="userGuess"
+      />
+
+      <div class="wrapper wrapper--range">
+        <label for="minInput">
+          Min
+        <input
+          v-model="min"
+          id="minInput"
+          class="input input--range"
+          type="number"
+          @blur="getNewRandom"
+        />
+        </label>
+
+        <label for="maxInput">
+          Max
+        <input
+          v-model="max"
+          id="maxInput"
+          class="input input--range"
+          type="number"
+          @blur="getNewRandom"
+        />
+        </label>
+      </div>
+
+      <div class="wrapper wrapper--buttons">
+        <button
+          @click="submitGuess"
+          class="btn btn--submit"
+          :disabled="!userGuess"
+        >
+          Submit
+        </button>
+
+        <button
+          @click="clearInput"
+          class="btn btn--clear"
+          :disabled="!userGuess"
+        >
+          Clear
+        </button>
+      </div>
+
+      <article
+        class="guess--output"
+        v-if="lastGuess"
+      >
+        Your last guess was
+        <span>{{lastGuess}}</span>
+      </article>
+      <article
+        class="guess--output"
+        v-if="!lastGuess"
+      >
+        <span>??</span>
+      </article>
+
+      <p
+        v-show="guessResult"
+        class="guess--result"
+      >
+        {{ guessResult }}
+      </p>
+
+      <button
+        @click="resetGame"
+        class="btn btn--reset"
+        :disabled="level <= 1"
+      >
+        Reset
+      </button>
+
+    </article>
+
   </section>
 </template>
 
 <script>
+const GUESS_ISNAN = 'That was not a number.  Please enter a number';
+const GUESS_ISHIGH = 'That was too high';
+const GUESS_ISLOW = 'That was too low';
+
 export default {
   name: 'Guesser',
+
   data() {
     return {
-      msg: 'Welcome to number guesser',
       userGuess: '',
       guessResult: '',
+      lastGuess: 0,
       min: 0,
       max: 100,
       rando: 0,
-      lastGuess: 0,
       level: 1,
     };
   },
-  computed: {
-    isResetDisabled() {
-      return this.$data.level === 1;
-    },
-  },
-  mounted: function getRandomOnMount() {
+
+  mounted() {
     this.getNewRandom();
   },
+
   methods: {
-    submitGuess: function submitGuess() {
-      this.evaluateGuess(this.userGuess);
+    submitGuess() {
+      this.evaluateGuess();
       this.clearInput();
+      this.$refs.userGuess.focus();
     },
 
-    clearInput: function clearInput() {
+    clearInput() {
       this.$data.userGuess = '';
     },
 
-    getNewRandom: function getRandom() {
-      const newRando = Math.round((Math.random() * (this.max - this.min)) + this.min);
-      this.$data.rando = newRando;
+    getNewRandom() {
+      const max = parseInt(this.max, 10);
+      const min = parseInt(this.min, 10);
+
+      if (min > max) {
+        this.guessResult = 'Please enter a valid range';
+        return null;
+      }
+
+      // eslint-disable-next-line
+      const newRando = Math.round(Math.random() * (max - min) + min);
+      this.rando = newRando;
+
+      return null;
     },
 
-    evaluateGuess: function evaluateGuess(guess) {
-      const intGuess = parseInt(guess, 10);
+    evaluateGuess() {
+      this.lastGuess = this.userGuess;
+      const intGuess = parseInt(this.userGuess, 10);
+      const { rando } = this;
       if (isNaN(intGuess)) {
-        this.badGuess('NaN');
+        this.guessResult = GUESS_ISNAN;
       }
-      if (intGuess > this.rando) {
-        this.badGuess('high');
+      if (intGuess > rando) {
+        this.guessResult = GUESS_ISHIGH;
       }
-      if (intGuess < this.rando) {
-        this.badGuess('low');
+      if (intGuess < rando) {
+        this.guessResult = GUESS_ISLOW;
       }
-      if (intGuess === this.rando) {
+      if (intGuess === rando) {
         this.gameWon();
       }
-      this.$data.lastGuess = intGuess;
     },
 
-    badGuess: function badGuess(feedback) {
-      if (feedback === 'high') {
-        this.$data.guessResult = 'your guess was too high';
-      }
-      if (feedback === 'low') {
-        this.$data.guessResult = 'your guess was too low';
-      }
-      if (feedback === 'NaN') {
-        this.$data.guessResult = 'you did not enter a number!';
-      }
-    },
-
-    gameWon: function gameWon() {
-      this.$data.guessResult = 'You Guessed It, min range decreased, max range increased!';
-      this.$data.min -= 10;
-      this.$data.max += 10;
-      this.$data.level += 1;
+    gameWon() {
+      this.guessResult = 'You Guessed It, min range decreased, max range increased!';
+      this.min -= 10;
+      this.max += 10;
+      this.level += 1;
       this.getNewRandom();
     },
 
-    resetGame: function resetGame() {
-      this.$data.lastGuess = 0;
-      this.$data.guessResult = '';
-      this.$data.min = 0;
-      this.$data.max = 100;
-      this.$data.level = 1;
+    resetGame() {
+      this.lastGuess = 0;
+      this.guessResult = '';
+      this.min = 0;
+      this.max = 100;
+      this.level = 1;
     },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,700');
+
+$primary-font: 'Open Sans', sans-serif;
+
+$color-background-dark: #404041;
+$color-primary-pink: #eb008b;
+$color-title-pink: #ed458b;
+$color-active-gray: #929497;
+$color-disabled-gray: #d0d2d3;
+$color-light-gray: #e6e7e8;
+$color-white: #fff;
+
+html {
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: $primary-font;
+  margin: 0;
+}
+
 h1,
 h2 {
   font-weight: normal;
@@ -121,7 +228,127 @@ li {
   margin: 0 10px;
 }
 
-a {
-  color: #42b983;
+.container {
+  max-width: 60vw;
+  margin: 0 auto;
+
+  @media screen and(max-width: 768px) {
+    max-width: 80vw;
+  }
+}
+
+.game-body {
+  display: flex;
+  flex-direction: column;
+}
+
+.message {
+  padding: 2rem;
+  background-color: $color-background-dark;
+  margin-bottom: 1rem;
+
+  .title {
+    margin: 0;
+    color: $color-title-pink;
+    font-size: 2.5rem;
+    font-weight: 700;
+    text-align: center;
+
+    span {
+      color: $color-light-gray;
+      font-weight: 300;
+    }
+  }
+
+  .range {
+    font-size: 1.5rem;
+    font-weight: 300;
+    color: $color-light-gray;
+    text-align: center;
+  }
+}
+
+.guess--output {
+  span {
+    font-size: 10rem;
+    color: $color-primary-pink;
+    font-weight: 300;
+    display: block;
+    line-height: 9rem;
+  }
+}
+
+.guess--output,
+.guess--result {
+  text-align: center;
+  font-size: 1.25rem;
+}
+
+.guess--result {
+  margin-top: 0.5rem;
+}
+
+.input {
+  font-size: 1.25rem;
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+
+  &--guess {
+    width: 100%;
+  }
+
+  &--range {
+    width: 100%;
+  }
+}
+
+.wrapper {
+  display: flex;
+
+  &--range {
+    justify-content: space-between;
+
+    label ~ label {
+      margin-left: 0.5rem;
+    }
+
+    @media screen and (min-width: 768px) {
+      justify-content: center;
+
+      label ~ label {
+        margin-left: 2rem;
+      }
+    }
+  }
+
+  &--buttons {
+    justify-content: space-around;
+
+    .btn {
+      width: 40%;
+    }
+  }
+}
+
+.btn {
+  background-color: $color-active-gray;
+  color: $color-white;
+  border: none;
+  border-radius: 1.25rem;
+  font-size: 1.25rem;
+  margin-bottom: 1rem;
+  padding: 0.5rem 1rem;
+
+  &:hover {
+    background-color: $color-primary-pink;
+  }
+
+  &:disabled {
+    background-color: $color-disabled-gray;
+  }
+
+  &--reset {
+    margin-top: 2rem;
+  }
 }
 </style>
